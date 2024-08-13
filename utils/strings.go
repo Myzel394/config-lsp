e8fe8d1a963e4ba8ef6a807babdcebbafc863e4d
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-08-13 22:36:50 +0200

    feat(utils): Add GetTrimIndex

--- utils/strings.go
+++ utils/strings.go
@@ -1,7 +1,22 @@
 package utils
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 func IndexOffset(s string, search string, start int) int {
 	return strings.Index(s[start:], search) + start
+}
+
+var trimIndexPattern = regexp.MustCompile(`^\s+(.+?)\s+`)
+
+func GetTrimIndex(s string) []int {
+	indexes := trimIndexPattern.FindStringSubmatchIndex(s)
+
+	if indexes == nil {
+		return nil
+	}
+
+	return indexes[2:4]
 }
