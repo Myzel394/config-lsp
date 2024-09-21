5b660d9b609a691b924567828ce4e1fed984b586
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-09-21 15:30:58 +0200

    feat(common): Add Position to common

--- common/location.go
+++ common/location.go
@@ -12,9 +12,58 @@ type Location struct {
 	Character uint32
 }
 
+func (l Location) GetRelativeIndexPosition(i IndexPosition) IndexPosition {
+	return i - IndexPosition(l.Character)
+}
+
+// LocationRange: Represents a range of characters in a document
+// Locations are zero-based, start-inclusive and end-exclusive
+// This approach is preferred over using an index-based range, because
+// it allows to check very easily for cursor positions, as well as for
+// index-based ranges.
 type LocationRange struct {
 	Start Location
 	End   Location
+}
+
+func (l LocationRange) ContainsPosition(p Position) bool {
+	return l.IsPositionAfterStart(p) && l.IsPositionBeforeEnd(p)
+}
+
+// Check if the given position is after the start of the range
+// It's like: Position >= Start
+// This checks inclusively
+func (l LocationRange) IsPositionAfterStart(p Position) bool {
+	return p.getValue() >= l.Start.Character
+}
+
+func (l LocationRange) IsPositionBeforeStart(p Position) bool {
+	return p.getValue() < l.Start.Character
+}
+
+// Check if the given position is before the end of the range
+// It's like: Position <= End
+// This checks inclusively
+func (l LocationRange) IsPositionBeforeEnd(p Position) bool {
+	switch p.(type) {
+	case CursorPosition:
+		return p.getValue() <= l.End.Character
+	case IndexPosition:
+		return p.getValue() < l.End.Character
+	}
+
+	return false
+}
+
+func (l LocationRange) IsPositionAfterEnd(p Position) bool {
+	switch p.(type) {
+	case CursorPosition:
+		return p.getValue() > l.End.Character
+	case IndexPosition:
+		return p.getValue() >= l.End.Character
+	}
+
+	return false
 }
 
 func (l LocationRange) ShiftHorizontal(offset uint32) LocationRange {
@@ -57,7 +106,7 @@ func (l LocationRange) ToLSPRange() protocol.Range {
 		},
 		End: protocol.Position{
 			Line:      l.End.Line,
-			Character: l.End.Character + 1,
+			Character: l.End.Character,
 		},
 	}
 }
@@ -65,10 +114,6 @@ func (l LocationRange) ToLSPRange() protocol.Range {
 func (l *LocationRange) ChangeBothLines(newLine uint32) {
 	l.Start.Line = newLine
 	l.End.Line = newLine
-}
-
-func (l LocationRange) ContainsCursor(line uint32, character uint32) bool {
-	return line == l.Start.Line && character >= l.Start.Character && character <= l.End.Character
 }
 
 func (l LocationRange) ContainsCursorByCharacter(character uint32) bool {
@@ -115,7 +160,57 @@ func CharacterRangeFromCtx(
 		},
 		End: Location{
 			Line:      line,
-			Character: end,
+			Character: end + 1,
 		},
 	}
+}
+
+type Position interface {
+	getValue() uint32
+}
+
+// Use this type if you want to use a cursor based position
+// A cursor based position is a position that represents a cursor
+// Given the example:
+// "PermitRootLogin yes"
+// Taking a look at the first character "P" - the index is 0.
+// However, the cursor can either be at:
+//
+//	"|P" - 0 or
+//	"P|" - 1
+//
+// This is used for example for textDocument/completion or textDocument/signature
+type CursorPosition uint32
+
+func (c CursorPosition) getValue() uint32 {
+	return uint32(c)
+}
+
+func (c CursorPosition) shiftHorizontal(offset uint32) CursorPosition {
+	return CursorPosition(uint32(c) + offset)
+}
+
+func LSPCharacterAsCursorPosition(character uint32) CursorPosition {
+	return CursorPosition(character)
+}
+
+func (c CursorPosition) IsBeforeIndexPosition(i IndexPosition) bool {
+	// |H[e]llo
+	return uint32(c) < uint32(i)
+}
+
+func (c CursorPosition) IsAfterIndexPosition(i IndexPosition) bool {
+	// H[e]|llo
+	return uint32(c) > uint32(i)+1
+}
+
+// Use this type if you want to use an index based position
+type IndexPosition uint32
+
+func (i IndexPosition) getValue() uint32 {
+	return uint32(i)
+}
+
+func LSPCharacterAsIndexPosition(character uint32) IndexPosition {
+	return IndexPosition(character)
 }
--- common/lsp.go
+++ common/lsp.go
@@ -1,5 +1,19 @@
 package common
 
+// LSPCharacterAsCursorPosition:
+// @deprecated
 func CursorToCharacterIndex(cursor uint32) uint32 {
 	return max(1, cursor) - 1
+}
+
+func DeprecatedImprovedCursorToIndex(
+	c CursorPosition,
+	line string,
+	offset uint32,
+) uint32 {
+	if len(line) == 0 {
+		return 0
+	}
+
+	return min(uint32(len(line)-1), uint32(c)-offset+1)
 }
