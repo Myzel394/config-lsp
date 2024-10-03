413d919719a57304a0eee0e612052ece3c8fd2f0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-10-03 14:27:05 +0200

    feat(ssh_config): Add hover support

--- handlers/ssh_config/lsp/text-document-hover.go
+++ handlers/ssh_config/lsp/text-document-hover.go
@@ -1,6 +1,10 @@
 package lsp
 
 import (
+	"config-lsp/common"
+	sshconfig "config-lsp/handlers/ssh_config"
+	"config-lsp/handlers/ssh_config/handlers"
+
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
@@ -9,5 +13,21 @@ func TextDocumentHover(
 	context *glsp.Context,
 	params *protocol.HoverParams,
 ) (*protocol.Hover, error) {
-	return nil, nil
+	line := params.Position.Line
+	index := common.LSPCharacterAsIndexPosition(params.Position.Character)
+
+	d := sshconfig.DocumentParserMap[params.TextDocument.URI]
+
+	option, _ := d.Config.FindOption(line)
+
+	if option == nil || option.Key == nil {
+		// Empty line
+		return nil, nil
+	}
+
+	return handlers.GetHoverInfoForOption(
+		option,
+		line,
+		index,
+	)
 }
