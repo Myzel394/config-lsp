3ac3ebbe50dfedbcbd7a6768b6f82ad0da6db0d4
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2025-02-16 15:04:52 +0100

    refactor(server): Outsource ssh_config code action: Add To Unknown into own file

--- server/handlers/ssh_config/handlers/fetch-code-actions.go
+++ server/handlers/ssh_config/handlers/fetch-code-actions.go
@@ -2,8 +2,6 @@ package handlers
 
 import (
 	sshconfig "config-lsp/handlers/ssh_config"
-	"config-lsp/handlers/ssh_config/diagnostics"
-	"fmt"
 
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
@@ -20,51 +18,4 @@ func FetchCodeActions(
 	actions = append(actions, getKeywordTypoFixes(d, params)...)
 
 	return actions
-}
-
-func getAddToUnknownCodeAction(
-	d *sshconfig.SSHDocument,
-	params *protocol.CodeActionParams,
-) []protocol.CodeAction {
-	line := params.Range.Start.Line
-
-	if unknownOption, found := d.Indexes.UnknownOptions[line]; found {
-		var blockLine *uint32
-
-		if unknownOption.Block != nil {
-			blockLineValue := uint32(unknownOption.Block.GetLocation().Start.Line)
-			blockLine = &blockLineValue
-		}
-
-		commandID := "sshconfig." + CodeActionAddToUnknown
-		command := protocol.Command{
-			Title:   fmt.Sprintf("Add %s to unknown options", unknownOption.Option.Key.Key),
-			Command: string(commandID),
-			Arguments: []any{
-				codeActionAddToUnknownArgs{
-					URI:        params.TextDocument.URI,
-					OptionLine: unknownOption.Option.Start.Line,
-					BlockLine:  blockLine,
-				},
-			},
-		}
-		kind := protocol.CodeActionKindQuickFix
-		codeAction := protocol.CodeAction{
-			Title:   fmt.Sprintf("Add %s to unknown options", unknownOption.Option.Key.Key),
-			Command: &command,
-			Kind:    &kind,
-			Diagnostics: []protocol.Diagnostic{
-				diagnostics.GenerateUnknownOption(
-					unknownOption.Option.Key.ToLSPRange(),
-					unknownOption.Option.Key.Value.Value,
-				),
-			},
-		}
-
-		return []protocol.CodeAction{
-			codeAction,
-		}
-	}
-
-	return nil
 }
