bda1fae85e21f66d29130d572a3d5f0dadbc7e26
Myzel394 <github.7a2op@simplelogin.co>
2025-07-09 22:39:17 +0200

    fix(server): Improve ArrayValue

--- server/doc-values/value-array.go
+++ server/doc-values/value-array.go
@@ -47,6 +47,10 @@ type ArrayValue struct {
 
 	// If true, array ArrayValue ignores the `Separator` if it's within quotes
 	RespectQuotes bool
+
+	// If true, the quotes will not be unwrapped and the raw string will be passed
+	// to the subvalue
+	PersistQuotes bool
 }
 
 func (v ArrayValue) GetTypeDescription() []string {
@@ -71,7 +75,7 @@ func (v ArrayValue) DeprecatedCheckIsValid(value string) []*InvalidValue {
 		values = strings.Split(value, v.Separator)
 	}
 
-	if *v.DuplicatesExtractor != nil {
+	if v.DuplicatesExtractor != nil && *v.DuplicatesExtractor != nil {
 		// Stores the values that are already found
 		valueSet := map[string]struct{}{}
 
@@ -125,14 +129,14 @@ func (v ArrayValue) DeprecatedCheckIsValid(value string) []*InvalidValue {
 }
 
 func (v ArrayValue) getCurrentValue(line string, cursor common.CursorPosition) (string, common.CursorPosition) {
-	index := common.DeprecatedImprovedCursorToIndex(cursor, line, 0)
+	index := max(uint32(cursor), 1) - 1
 
 	if line == "" {
 		return line, 0
 	}
 
 	MIN := uint32(0)
-	MAX := uint32(len(line) - 1)
+	MAX := uint32(len(line))
 
 	var indexSearchStart = index
 	var indexSearchEnd = index
@@ -169,43 +173,74 @@ func (v ArrayValue) getCurrentValue(line string, cursor common.CursorPosition) (
 	)
 
 	if found {
+		// Edge case, two separators in a row
+		// -> value is empty
+		if relativePosition < (len(line)-1) && line[relativePosition+1] == v.Separator[0] {
+			return "", 0
+		}
+
+		// Edge case, separator at start
+		if relativePosition == 0 && cursor == 0 {
+			return "", 0
+		}
+
 		// + 1 to skip the separator
 		start = min(
 			MAX,
 			uint32(relativePosition)+1,
 		)
+		indexSearchEnd += 1
 	} else {
 		start = MIN
 	}
 
-	relativePosition, found = utils.FindNextCharacter(
-		line,
-		v.Separator,
-		int(indexSearchEnd),
-	)
-
-	if found {
-		// - 1 to skip the separator
-		end = max(
-			MIN,
-			uint32(relativePosition)-1,
-		)
-	} else {
+	if indexSearchEnd >= uint32(len(line)) {
 		end = MAX
+	} else {
+
+		relativePosition, found = utils.FindNextCharacter(
+			line,
+			v.Separator,
+			int(indexSearchEnd),
+		)
+
+		if found {
+			// - 1 to skip the separator
+			end = max(
+				MIN,
+				uint32(relativePosition)-1,
+			) + 1
+		} else {
+			end = MAX
+		}
+
 	}
 
-	if index > end {
-		// The user is typing a new (yet empty) value
-		return "", 0
+	return line[start:end], cursor.ShiftHorizontal(-start)
+}
+
+func (v ArrayValue) unwrapQuotes(value string, cursor common.CursorPosition) (string, common.CursorPosition) {
+	if v.RespectQuotes && !v.PersistQuotes && len(value) >= 1 {
+		newValue := strings.TrimSuffix(strings.TrimPrefix(value, "\""), "\"")
+
+		if value[0] == '"' && cursor <= 1 {
+			return newValue, 0
+		}
+
+		if value[len(value)-1] == '"' && uint32(cursor) >= uint32(len(value))-1 {
+			return newValue, common.CursorPosition(uint32(len(value)))
+		}
 	}
 
-	return line[start : end+1], cursor.ShiftHorizontal(-start)
+	// Nothing to do
+	return value, cursor
 }
 
 func (v ArrayValue) FetchCompletions(value string, cursor common.CursorPosition) []protocol.CompletionItem {
 	relativeValue, relativeCursor := v.getCurrentValue(value, cursor)
+	newValue, newCursor := v.unwrapQuotes(relativeValue, relativeCursor)
 
-	return v.SubValue.FetchCompletions(relativeValue, relativeCursor)
+	return v.SubValue.FetchCompletions(newValue, newCursor)
 }
 
 func (v ArrayValue) DeprecatedFetchHoverInfo(line string, cursor uint32) []string {
