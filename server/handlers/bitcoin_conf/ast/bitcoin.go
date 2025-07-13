539a6d2280d31a207bc51b2c74b48e25404ce7d9
Myzel394 <github.7a2op@simplelogin.co>
2025-07-13 22:48:55 +0200

    feat(server/bitcoin): Add fields

---
+++ server/handlers/bitcoin_conf/ast/bitcoin.go
@@ -0,0 +1,27 @@
+package ast
+
+import (
+	"config-lsp/common"
+	"config-lsp/parsers/ini"
+)
+
+type BTCConfig struct {
+	*ini.Config
+}
+
+// NewBTCConfig creates a new Bitcoin configuration
+func NewBTCConfig() *BTCConfig {
+	return &BTCConfig{
+		Config: ini.NewConfig(),
+	}
+}
+
+// Reset the configuration
+func (c *BTCConfig) Clear() {
+	c.Config.Clear()
+}
+
+// Parse a Bitcoin configuration string
+func (c *BTCConfig) Parse(input string) []common.LSPError {
+	return c.Config.Parse(input)
+}
