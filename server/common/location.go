e52eb92e40f89be78691f98e3054a11a629b0b7e
Myzel394 <github.7a2op@simplelogin.co>
2025-07-15 23:04:15 +0200

    feat(server): Add IncludeNext to LocationRange

--- server/common/location.go
+++ server/common/location.go
@@ -98,6 +98,17 @@ var GlobalLocationRange = LocationRange{
 	},
 }
 
+// This will include the next line, so the end will be at the start of the next line
+func (l LocationRange) IncludeNextLine() LocationRange {
+	return LocationRange{
+		Start: l.Start,
+		End: Location{
+			Line:      l.End.Line + 1,
+			Character: 0,
+		},
+	}
+}
+
 func (l LocationRange) ToLSPRange() protocol.Range {
 	return protocol.Range{
 		Start: protocol.Position{
