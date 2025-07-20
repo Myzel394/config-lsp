9aaf2de2fd7c366c4eb2995ecfaee32ad5a33976
Myzel394 <github.7a2op@simplelogin.co>
2025-07-20 20:43:35 +0200

    fix: Fix build

--- server/common/common.go
+++ server/common/common.go
@@ -2,4 +2,4 @@ package common
 
 // The comment below at the end of the line is required for the CI:CD to work.
 // Do not remove it
-var Version = "0.3.0" // CI:CD-VERSION
+var Version = "0.3.1" // CI:CD-VERSION
