fd8abbdb198f324d2db187145a2073f5c225d200
Myzel394 <github.7a2op@simplelogin.co>
2025-07-25 20:35:43 +0200

    feat(website): Add first design

---
+++ website/tailwind.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1E40AF', // Example primary color
+            }
+        }
+    },
+    plugins: [],
+}
